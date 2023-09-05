a6e6a0c95dca9e527e4dbc8dd72a3a75a2bcc23e
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-09-05 16:13:15 -0700

    Preview of Form components (#4)
    * Progress on announcement_form componenet
    
    * Initial WIP for Announcement Form, work on Event Form next
    
    * Added character limit to title/content
    
    * Added transitions to event cards

---
+++ src/lib/components/announcement_form.svelte
@@ -0,0 +1,129 @@
+<script lang="ts">
+	// TODO(Ethan): Figure out how to import from $lib/clubannounce.
+	// import type { AnnouncementForm } from '$lib/clubannounce/clubannounce';
+
+	// export let data: ClubannounceEvent;
+
+	/**
+	 * Render the card instead of the HTML form.
+	 */
+	export let readonly = false;
+	const CHAR_LIMIT = 2000;
+
+	export let isOpen: boolean;
+
+	let title = '';
+	let content = '';
+	let startDate: Date;
+
+	$: characterCount = title.length + content.length;
+</script>
+
+<form class="announcement-card" class:active={isOpen}>
+	<h2 class="m-bottom">Create a New Announcement</h2>
+	<div class="column m-bottom">
+		<label for="title">Title</label>
+		<input name="title" type="text" bind:value={title} />
+	</div>
+
+	<div class="column">
+		<textarea name="content" bind:value={content} placeholder="Start typing here..." />
+	</div>
+
+	<div class="column col-right m-bottom">
+		<span class:limit-exceeded={characterCount > CHAR_LIMIT}>{characterCount}</span>/2000 characters
+	</div>
+
+	{#if !readonly}
+		<div class="column col-right">
+			<button class="save" type="submit">Save</button>
+			<button class="create" type="submit">Create</button>
+		</div>
+	{/if}
+</form>
+
+<style lang="scss">
+	.announcement-card {
+		z-index: 10;
+		position: absolute;
+		left: 50%;
+		top: 55%;
+		transform: translate(-50%, -50%);
+		display: grid;
+		flex-direction: column;
+		width: min(50em, 90vw);
+		padding: 2rem;
+		background-color: var(--acm-light);
+		opacity: 0;
+		visibility: hidden;
+		transition: all 0.125s ease-in;
+
+		&.active {
+			top: 50%;
+			opacity: 1;
+			visibility: visible;
+		}
+
+		& .column {
+			&.col-right {
+				justify-self: end;
+			}
+		}
+
+		& .m-bottom {
+			margin-block-end: 1rem;
+		}
+
+		& h2 {
+			margin-block: 0;
+			font-size: 1.75rem;
+		}
+
+		& textarea {
+			width: 100%;
+			height: 10rem;
+			resize: none;
+		}
+
+		& input[type='text'],
+		textarea {
+			background-color: var(--acm-canvas);
+			font-size: 1rem;
+			padding: 0.5rem 1rem;
+			outline: none;
+			border: none;
+			width: 100%;
+			// text-align: left;
+		}
+
+		.save,
+		.create {
+			outline: none;
+			border: none;
+			cursor: pointer;
+			font-size: 1rem;
+			border-radius: 0.5rem;
+		}
+
+		.save {
+			padding: 0.75rem;
+			background-color: transparent;
+			color: rgb(var(--acm-general-rgb));
+		}
+
+		.create {
+			padding: 0.75rem 1.5rem;
+			background-color: rgb(var(--acm-general-rgb));
+			color: var(--acm-light);
+			transition: all 0.2s ease-in;
+
+			&:hover {
+				opacity: 0.7;
+			}
+		}
+	}
+
+	.limit-exceeded {
+		color: var(--acm-red);
+	}
+</style>
--- src/lib/components/event_form.svelte
+++ src/lib/components/event_form.svelte
@@ -1,13 +1,98 @@
 <script lang="ts">
-	// TODO(Ethan): Figure out how to import from $lib/clubannounce.
-	import type { ClubannounceEvent } from '$lib/clubannounce/clubannounce';
-
-	export let data: ClubannounceEvent;
-
-	/**
-	 * Render the card instead of the HTML form.
-	 */
-	export let readonly = false;
+	export let isOpen: boolean;
+	let startDate: Date;
 </script>
 
-EventForm
+<form class="event-card" class:active={isOpen}>
+	<h2>Create a New Event</h2>
+	<div class="column">
+		<label for="start-time">Start Date</label>
+		<input name="start-time" type="date" bind:value={startDate} />
+
+		<label for="start-time">Start Date</label>
+		<input name="start-time" type="date" bind:value={startDate} />
+	</div>
+	<div class="column">
+		<label for="start-time">Start Date</label>
+		<input name="start-time" type="date" bind:value={startDate} />
+
+		<label for="start-time">Start Date</label>
+		<input name="start-time" type="date" bind:value={startDate} />
+	</div>
+	<div class="column col-right">
+		<button class="save-event" type="submit">Save</button>
+		<button class="create-event" type="submit">Create</button>
+	</div>
+</form>
+
+<style lang="scss">
+	.event-card {
+		z-index: 10;
+		position: absolute;
+		left: 50%;
+		top: 55%;
+		transform: translate(-50%, -50%);
+		display: flex;
+		flex-direction: column;
+		width: min(50em, 90vw);
+		padding: 2rem;
+		background-color: var(--acm-light);
+		opacity: 0;
+		visibility: hidden;
+		transition: all 0.125s ease-in;
+
+		&.active {
+			top: 50%;
+			opacity: 1;
+			visibility: visible;
+		}
+
+		& .column {
+			margin-block-end: 2rem;
+			&.col-right {
+				justify-self: end;
+			}
+		}
+
+		& h2 {
+			margin-block: 0;
+			margin-block-end: 2rem;
+		}
+
+		& input[type='date'] {
+			background-color: var(--acm-canvas);
+			font-size: 1rem;
+			padding: 1rem;
+			outline: none;
+			border: none;
+			text-align: right;
+			width: 35%;
+		}
+
+		.save-event,
+		.create-event {
+			outline: none;
+			border: none;
+			cursor: pointer;
+			border-radius: 0.5rem;
+			font-size: 1rem;
+		}
+
+		.save-event {
+			padding: 0.75rem;
+			background-color: transparent;
+			color: rgb(var(--acm-general-rgb));
+		}
+
+		.create-event {
+			padding: 0.75rem 1.5rem;
+			background-color: rgb(var(--acm-general-rgb));
+			color: var(--acm-light);
+			transition: all 0.2s ease-in;
+
+			&:hover {
+				opacity: 0.7;
+			}
+		}
+	}
+</style>
